9625a9e0028149ed4d1bee7fff61de3692d09c39
Dan G <daniel.w.gray1994@gmail.com>
2024-01-22 13:25:05 -0500

    changed execute function to accept multiple arguments and close the database connection once the query is executed

--- execute.py
+++ execute.py
@@ -5,8 +5,24 @@ connection = sqlite3.connect('practice.db')
 crsr = connection.cursor()
 
 
-def execute(query):
-    return crsr.execute(query)
+def execute(query, *args, fetchall=False):
+    connection = sqlite3.connect('practice.db')
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute(query, args)
     
-def close():
-    connection.close()
+        connection.commit()
+
+        if fetchall:
+            results = cursor.fetchall()
+            return results
+    
+    except sqlite3.Error as error:
+        print(f"Error executing query: {error}")
+    
+    finally:
+        cursor.close()
+        connection.close()
+    
+    
--- index.py
+++ index.py
@@ -1,6 +1,12 @@
-from execute import execute, close
+from execute import execute
+
+id = 1
+username = "Dan"
+password = "Gray"
+
+execute("DROP TABLE IF EXISTS users;")
 
 execute("CREATE TABLE users (id INTEGER NOT NULL, username TEXT NOT NULL, password TEXT NOT NULL);")
 
-close()
+execute("INSERT INTO users VALUES (?, ?, ?)", id, username, password)
 
