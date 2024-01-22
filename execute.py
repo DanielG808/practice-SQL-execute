52e05d5c8ff8a264099bf3a619ad0ae734a8e5d4
Dan G <daniel.w.gray1994@gmail.com>
2024-01-22 12:52:50 -0500

    added two helper functions that were used to create a sqlite db

--- execute.py
+++ execute.py
@@ -4,6 +4,9 @@ connection = sqlite3.connect('practice.db')
 
 crsr = connection.cursor()
 
+
 def execute(query):
     return crsr.execute(query)
     
+def close():
+    connection.close()
---
+++ index.py
@@ -0,0 +1,6 @@
+from execute import execute, close
+
+execute("CREATE TABLE users (id INTEGER NOT NULL, username TEXT NOT NULL, password TEXT NOT NULL);")
+
+close()
+
